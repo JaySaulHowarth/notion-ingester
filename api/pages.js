cff10e5df1da5849597c48b6bd2478bf1ef1de5f
Jay Howarth <jayhowarthmbro@gmail.com>
2023-01-08 21:00:06 +0000

    Added getPages function
    getPages iterates over an array of page IDs and calls getPage on them

--- api/pages.js
+++ api/pages.js
@@ -3,7 +3,7 @@
  */
 
 /**
- * Retrieve a singe Notion page using its page ID.
+ * Retrieve a singe Notion page.
  * @param notion Client used to send authenticated requests
  * @param {string} pageId Page ID for the Notion page being retrieved
  * @return Returns a promise that resolves to a Notion page
@@ -12,4 +12,26 @@ const getPage = function(notion, pageId) {
     return notion.pages.retrieve({page_id: pageId});
 }
 
-module.exports = { getPage }
+/**
+ * Retrieve a multiple Notion pages.
+ * @param notion Client used to send authenticated requests
+ * @param {string} pageIds Array of page IDs for the Notion pages being retrieved
+ * @return Returns an array of pages
+ */
+const getPages = function(notion, pageIds) {
+    const pages = [];
+
+    for(i=0, n=pageIds.length; i < n; i++) {
+        const page = getPage(notion, pageIds[i])
+        pages.push(page);
+    }
+
+    Promise.all(pages).then((pages) => {
+        return pages
+    })
+}
+
+module.exports = { 
+    getPage,
+    getPages
+}
