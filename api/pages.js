b4fbe6c8416af470914e67e6ff331b1d8c4a3534
Jay Howarth <jayhowarthmbro@gmail.com>
2023-01-09 20:05:57 +0000

    Added getPageContent and getPageProperties
    Combined these functions can be used to get a pages properties and content

--- api/pages.js
+++ api/pages.js
@@ -3,20 +3,52 @@
  */
 
 /**
- * Retrieve a singe Notion page.
+ * Retrieve a single Notion page's properties.
  * @param notion Client used to send authenticated requests
  * @param {string} pageId Page ID for the Notion page being retrieved
- * @return Returns a promise that resolves to a Notion page
+ * @return Returns a promise that resolves to a pages properties
  */
-const getPage = function(notion, pageId) {
+const getPageProperties = function(notion, pageId) {
     return notion.pages.retrieve({page_id: pageId});
 }
 
 /**
- * Retrieve a multiple Notion pages.
+ * Retrieve a single Notion page's content (block children).
+ * @param {*} notion Client used to send authenticated requests
+ * @param {string} pageId Page ID for the Notion page being retrieved
+ * @return Returns a promise that resolves to a pages content (block children)
+ */
+const getPageContent = function(notion, pageId) {
+    return notion.blocks.children.list({
+        block_id: pageId,
+        page_size: 50
+    });
+}
+
+/**
+ * Retrieve a Notion page (properties & block children)
+ * @param {*} notion 
+ * @param {string} pageId 
+ * @return Returns a page object
+ */
+const getPage = async function(notion, pageId) {
+    var page = {
+        properties: "",
+        content: ""
+    }
+
+    page.properties = await getPageProperties(notion, pageId);
+    page.content = await getPageContent(notion,pageId);
+
+    return page;
+    
+}
+
+/**
+ * Retrieve properties from multiple Notion pages.
  * @param notion Client used to send authenticated requests
  * @param {string} pageIds Array of page IDs for the Notion pages being retrieved
- * @return Returns an array of pages
+ * @return Returns an array of page properties
  */
 const getPages = function(notion, pageIds) {
     const pages = [];
@@ -28,10 +60,14 @@ const getPages = function(notion, pageIds) {
 
     Promise.all(pages).then((pages) => {
         return pages
+    }).catch((e) => {
+        console.error(e.message, e)
     })
 }
 
 module.exports = { 
     getPage,
-    getPages
+    getPages,
+    getPageContent,
+    getPageProperties
 }
